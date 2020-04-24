7d3577cea4e2a67cc1cfdc39919762fd4a4341c3
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-24 21:46:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,10 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |name, value|
     if value == nil || value < min_value
+      min_key = name
+      min_value = value
     end
   end
+
+  min_vale
 end
